412169b5d651a024bd807b5692bb7483466e3e20
Jyeverson Madalozo <jyeversonmadalozodev@gmail.com>
2021-09-17 19:53:24 -0300

    Project structure created

---
+++ android/src/com/flappybemtevi/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.flappybemtevi;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.flappybemtevi.BemTeVi;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BemTeVi(), config);
+	}
+}
