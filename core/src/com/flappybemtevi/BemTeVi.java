a272ed7815bc439dd16cc88fb215b5ca5c9cf5ef
Jyeverson Madalozo <jyeversonmadalozodev@gmail.com>
2021-09-18 09:32:25 -0300

    Adding new changes

--- core/src/com/flappybemtevi/BemTeVi.java
+++ core/src/com/flappybemtevi/BemTeVi.java
@@ -1,31 +1,43 @@
 package com.flappybemtevi;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class BemTeVi extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture obstaculoBaixo;
+
+
+	Float posicaoX = 0f;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		obstaculoBaixo = new Texture("canobaixo.png");
+		posicaoX = (float) Gdx.graphics.getWidth();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		posicaoX -= Gdx.graphics.getDeltaTime()*500;
+
+		if(posicaoX <- obstaculoBaixo.getWidth()) {
+			posicaoX = (float) Gdx.graphics.getWidth();
+		}
+
+		batch.draw(obstaculoBaixo, posicaoX, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		obstaculoBaixo.dispose();
 	}
 }
